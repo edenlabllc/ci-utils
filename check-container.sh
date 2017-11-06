a6f165eaf9cbb1c845a9c5c27e9672db31d0eb68
ybod <yuriibodarev@gmail.com>
2017-11-06 14:52:25 +0200

    check-container permissions set

--- check-container.sh
+++ check-container.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # This script can check if a local Docker container with created image started succesfully.
-
 RUNNING_CONTAINERS=$(docker ps | wc -l | tr -d '[:space:]');
 
 if [ $RUNNING_CONTAINERS == "1" ]; then
-  echo "[E] Container is not started\!";
+  echo "[E] Container is not started.";
   docker logs ${PROJECT_NAME} --details --since 5h;
   exit 1;
 fi;
