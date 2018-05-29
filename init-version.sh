3ac541d654ad9613b907306c76f50ec2ce681247
Alex Kovalevych <alexkovalevych@gmail.com>
2018-05-30 00:56:22 +0300

    define docker hub version - WIP

--- init-version.sh
+++ init-version.sh
@@ -5,8 +5,16 @@ echo "Retrieving token ..."
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_HUB_LOGIN}'", "password": "'${DOCKER_HUB_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
 
 # get list of repositories
-echo "Retrieving repository list ..."
-REPO_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORG}/?page_size=100 | jq -r '.results|.[]|.name')
+APPS_LIST=$(echo ${APPS} | jq -r '.[]')
+echo ${APPS_LIST}
+for i in ${APPS_LIST}
+do
+  echo "${i}:"
+  # tags
+  IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DOCKER_HUB_LOGIN}/${i}/tags/?page_size=100 | jq -r '.results|.[]|.name')
+  echo ${IMAGE_TAGS}
+  echo
+done
 
 if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
   DOCKER_HUB_TAG="develop"
