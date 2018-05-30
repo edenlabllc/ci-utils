c424c829c8e73754b333893e69d6466141c4ec1d
Alex Kovalevych <alexkovalevych@gmail.com>
2018-05-30 16:58:22 +0300

    build container

---
+++ build-container.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script builds an image based on a Dockerfile that is located in root of git working tree.
+set -e
+
+if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
+    APPS_LIST=$(echo ${APPS} | jq -r '.[]')
+    for i in ${APPS_LIST}
+    do
+        echo "${i}:"
+        echo "[I] Building a Docker container '${i}' from path '${PROJECT_DIR}'.."
+    docker build --tag "${i}:${PROJECT_VERSION}" \
+                --file "${PROJECT_DIR}" \
+                --build-arg APP_NAME=$i \
+                "$PROJECT_DIR"
+
+    echo
+    done
+fi;
--- init-version.sh
+++ init-version.sh
@@ -1,33 +1,31 @@
+#!/bin/bash
+set -e
+
 export PROJECT_DIR=${TRAVIS_BUILD_DIR:=$PWD}
 
 # Get latest version
 echo "Retrieving token ..."
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_HUB_LOGIN}'", "password": "'${DOCKER_HUB_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
 
+export TOKEN=${TOKEN}
 # get list of repositories
-APPS_LIST=$(echo ${APPS} | jq -r '.[]')
-echo ${APPS_LIST}
-for i in ${APPS_LIST}
-do
-  echo "${i}:"
-  # tags
-  IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DOCKER_HUB_LOGIN}/${i}/tags/?page_size=100 | jq -r '.results|.[]|.name')
-  echo ${IMAGE_TAGS}
-  echo
-done
+# APPS_LIST=$(echo ${APPS} | jq -r '.[]')
+# for i in ${APPS_LIST}
+# do
+#   echo "${i}:"
+#   tags
+#   IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DOCKER_HUB_LOGIN}/${i}/tags/?page_size=100 | jq -r '.results|.[]|.name')
+#   echo ${IMAGE_TAGS}
+#   echo
+# done
 
-if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
-  DOCKER_HUB_TAG="develop"
-# elif [[ ${PATCH_CHANGES} == "0" ]]; then
-#   NEXT_PATCH_VERSION=$(expr ${parts[2]} + 1)
-# else
-#   NEXT_PATCH_VERSION=$(expr ${parts[2]} + ${PATCH_CHANGES})
-fi;
+# if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
+#   DOCKER_HUB_TAG="develop"
+# fi;
 
+# # Show version info
+# echo
+# echo "Version information: "
+# echo " - Next version will be ${DOCKER_HUB_TAG}"
 
-# Show version info
-echo
-echo "Version information: "
-echo " - Next version will be ${DOCKER_HUB_TAG}"
-
-export DOCKER_HUB_TAG=$DOCKER_HUB_TAG
+# export DOCKER_HUB_TAG=$DOCKER_HUB_TAG
