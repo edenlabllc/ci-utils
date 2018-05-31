912d8d1a4843ac526de7ce9b193ebae66c93d0e3
Alex Kovalevych <alexkovalevych@gmail.com>
2018-05-31 12:54:02 +0300

    add coverage

--- tests.sh
+++ tests.sh
@@ -4,30 +4,29 @@ cd $TRAVIS_BUILD_DIR
 
 # Run all tests except pending ones
 echo "- mix test --exclude pending --trace "
-RES=$(mix test --exclude pending --trace)
-echo ${RES}
+mix test --exclude pending --trace
+is_failed=0;
 
 if [ "$?" -eq 0 ]; then
   echo "mix test successfully completed"
 else
   echo "mix test Finished with errors ,exited with 1"
-  mix_test=1 ;
+  is_failed=1 ;
 fi;
 
 # Submit code coverage report to Coveralls
 # Add --pro if you using private repo.
-# echo "COVERALLS: ${COVERALLS}"
-# if [ ${COVERALLS} = true ]; then
-#   echo "- mix coveralls.travis --exclude pending --umbrella;"
-#         mix coveralls.travis --exclude pending --umbrella
+if [ ${COVERALLS} ]; then
+  echo "- mix coveralls.travis --exclude pending --umbrella;"
+        mix coveralls.travis --exclude pending --umbrella
 
-#       if [ "$?" -eq 0 ]; then
-#             echo "mix coveralls.travis successfully completed"
-#           else
-#             echo "mix coveralls.travis finished with errors , exited with 1"
-#             mix_test=1;
-#       fi;
-# fi;
+      if [ "$?" -eq 0 ]; then
+            echo "mix coveralls.travis successfully completed"
+          else
+            echo "mix coveralls.travis finished with errors , exited with 1"
+            is_failed=1;
+      fi;
+fi;
 
 # Run static code analysis
 echo "- mix credo --strict ; "
@@ -37,7 +36,7 @@ if [ "$?" -eq 0 ]; then
   echo "mix credo successfully completed"
 else
   echo "mix credo finished with errors, exited with 1"
-  mix_test=1;
+  is_failed=1;
 fi;
 
 # Check code style
@@ -48,10 +47,10 @@ if [ "$?" -eq 0 ]; then
   echo "mix format successfully completed"
 else
   echo "mix format finished with errors, exited with 1"
-  mix_test=1;
+  is_failed=1;
 fi;
 
-if [ "${mix_test}" == "1" ]; then
+if [ "${is_failed}" -eq 1 ]; then
   echo "finished with errors"
   exit 1;
 fi;
