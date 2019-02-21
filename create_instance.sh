19724196e939c004498f21b9f05fbefa6b393281
Dima Hlebnikov <lakone41@gmail.com>
2019-02-21 16:19:49 +0200

    add exit if exit 0

--- create_instance.sh
+++ create_instance.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 for ((i = 0; i < 10; i++))
 do 
-    gcloud container node-pools create ehealth-build-${BUILD_NUMBER} --cluster=dev --machine-type=n1-highcpu-16 --node-taints=ci=${BUILD_TAG}:NoSchedule --node-labels=node=${BUILD_TAG} --num-nodes=1 --zone=europe-west1-d --preemptible || FAIL=1
-    if [ $FAIL == 1 ]; then
+    gcloud container node-pools create ehealth-build-${BUILD_NUMBER} --cluster=dev --machine-type=n1-highcpu-16 --node-taints=ci=${BUILD_TAG}:NoSchedule --node-labels=node=${BUILD_TAG} --num-nodes=1 --zone=europe-west1-d --preemptible
+    if [ $? != 0 ]; then
     sleep 25
     continue
+    else
+    break
     fi
 
 done
