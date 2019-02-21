04b133cda2009feb12f07b37a89c66940ca4273e
Dima Hlebnikov <lakone41@gmail.com>
2019-02-21 17:25:34 +0200

    add exit if all expires

--- create_instance.sh
+++ create_instance.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-for ((i = 0; i < 10; i++))
+for ((i = 1; i < 11; i++))
 do 
     gcloud container node-pools create ehealth-build-${BUILD_NUMBER} --cluster=dev --machine-type=n1-highcpu-16 --node-taints=ci=${BUILD_TAG}:NoSchedule --node-labels=node=${BUILD_TAG} --num-nodes=1 --zone=europe-west1-d --preemptible
-    if [ $? != 0 ]; then
-    sleep 25
-    continue
-    else
-    break
-    fi
-
+        if [ $? -ne 0 ]; then
+            sleep 25
+            if [ $i -eq 10 ]; then
+                echo could not create instance
+                exit 1
+            fi  
+        else
+            break
+        fi
 done
