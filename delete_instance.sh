131906b81e6357ac38f81974f05b1015e84d59d1
Dima Hlebnikov <lakone41@gmail.com>
2019-02-22 14:34:23 +0200

    fix delete instance

--- delete_instance.sh
+++ delete_instance.sh
@@ -2,8 +2,8 @@
 echo "------start script-----"
 while true 
 do
-    sleep 1
-    if gcloud container node-pools delete ehealth-build-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet | grep -q 'Not found'; then
+    sleep 4
+    if gcloud container node-pools delete ehealth-build-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet | grep -q 'not found'; then
         echo Delete instans succesfull
         exit 0
     fi
