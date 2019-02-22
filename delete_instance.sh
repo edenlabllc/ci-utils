9f13b2392f22fb127c3d540cc4c8fd2bcac815be
liubenokvlad <liubenok@edenlab.com.ua>
2019-02-22 13:00:08 +0200

    [ci skip] quick update

--- delete_instance.sh
+++ delete_instance.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "------start script-----"
 until gcloud container node-pools delete ehealth-build-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet;
 do
-    sleep 22
+    sleep 25
+    echo "there are no file ff" 
+    if echo "$?" | grep -q "ERROR: (gcloud.container.node-pools.delete) ResponseError: code=404"; then
+        echo "We found error ERROR: (gcloud.container.node-pools.delete) ResponseError: code=404"
+        break
+    fi  
 done
