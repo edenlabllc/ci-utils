cb7cae13cffb711543f10d8325cdc6bcf1d70d76
liubenokvlad <liubenok@edenlab.com.ua>
2019-02-22 13:34:21 +0200

    [ci skip] quick update

--- delete_instance.sh
+++ delete_instance.sh
@@ -2,8 +2,7 @@
 echo "------start script-----"
 until gcloud container node-pools delete ehealth-build-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet;
 do
-    sleep 3
-    echo "there are no file ff" 
+    sleep 3 
     if  grep -q "Not found"; then
         echo "We found error ERROR: (gcloud.container.node-pools.delete) ResponseError: code=404, message=Not found: node pool"
         break
