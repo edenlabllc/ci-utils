99902b5388a897ca2604c2cfd0ea26a87cb513c0
Dima Hlebnikov <lakone41@gmail.com>
2019-02-23 18:06:39 +0200

    [ci skip] change instance-pool name to fit-commit hash

--- delete_instance.sh
+++ delete_instance.sh
@@ -3,7 +3,7 @@ echo "------start script-----"
 while true 
 do
     sleep 4
-    if gcloud container node-pools delete ${PROJECT_NAME}-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q 'Not found'; then
+    if gcloud container node-pools delete e-${GIT_COMMIT}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q 'Not found'; then
         echo Instance doesnt exist
         exit 0
     fi
