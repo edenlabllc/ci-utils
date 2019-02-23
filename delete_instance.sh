0d8c02d704b263cf36d99cfd5842eadaa5a5382e
Dima Hlebnikov <lakone41@gmail.com>
2019-02-23 22:28:44 +0200

    change delete instace

--- delete_instance.sh
+++ delete_instance.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "------start script-----"
-while true 
+EX=0
+while [ $EX -eq 0 ] 
 do
-    sleep 4
     if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q 'Not found'; then
-        echo Instance doesnt exist
-        exit 0
+        sleep 4
+        echo "Instance doesnt exist"
+        EX=1
+    else
+        sleep 4
+        echo "Try to delete instance"
     fi
 done
