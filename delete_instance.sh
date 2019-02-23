1ad2d3836e44e974961002eeb30027e3e75fb28f
Dima Hlebnikov <lakone41@gmail.com>
2019-02-23 23:40:06 +0200

    add another if

--- delete_instance.sh
+++ delete_instance.sh
@@ -3,7 +3,9 @@ echo "------start script-----"
 EX=0
 until [ $EX -eq 1 ] 
 do
-    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
+    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet; then
+        echo "Intsance deleted"
+    elif gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet | grep -q "Not found"; then
         sleep 4
         echo "Instance doesnt exist"
         EX=1
