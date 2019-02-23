2db7102ce0a556b02fafd048abae47f2edbabcd4
Dima Hlebnikov <lakone41@gmail.com>
2019-02-23 23:05:00 +0200

    try to use until

--- delete_instance.sh
+++ delete_instance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "------start script-----"
 EX=0
-while [ $EX -eq 0 ] 
+until [ $EX -eq 1 ] 
 do
-    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q 'Not found'; then
+    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
         sleep 4
         echo "Instance doesnt exist"
         EX=1
