b40b4aacb09e65a42d1abc3c0f28b0f022a4f1e0
Dima Hlebnikov <lakone41@gmail.com>
2019-02-24 12:08:58 +0200

    [ci skip] add instance type to create and change instance delete

--- create_instance.sh
+++ create_instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-until gcloud container node-pools create ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --cluster=dev --machine-type=n1-highcpu-16 --node-taints=ci=${BUILD_TAG}:NoSchedule --node-labels=node=${BUILD_TAG} --num-nodes=1 --zone=europe-west1-d --preemptible;
+until gcloud container node-pools create ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${BUILD_TAG}:NoSchedule --node-labels=node=${BUILD_TAG} --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
 done
--- delete_instance.sh
+++ delete_instance.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 echo "------start script-----"
-EX=0
-GIT_COMM=${GIT_COMMIT:0:7}
-until [ $EX -eq 1 ] 
+until gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"
 do
-    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMM}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet; then
-        echo "Intsance deleted"
-    elif gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMM}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
-        sleep 4
-        echo "Instance doesnt exist"
-        EX=1
-    else
-        sleep 4
-        echo "Try to delete instance"
-    fi
+ sleep 4
 done
