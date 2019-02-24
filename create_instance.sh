7f9c0432643ff1e9009c5750eef4ee6cbd624483
Dima Hlebnikov <lakone41@gmail.com>
2019-02-24 16:17:17 +0200

    [ci skip] remove change-id from naiming for instances

--- create_instance.sh
+++ create_instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-until gcloud container node-pools create ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${BUILD_TAG}:NoSchedule --node-labels=node=${BUILD_TAG} --num-nodes=1 --zone=europe-west1-d --preemptible;
+until gcloud container node-pools create ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${BUILD_TAG}:NoSchedule --node-labels=node=${BUILD_TAG} --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
 done
