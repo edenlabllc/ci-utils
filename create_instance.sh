c3940f6fa018269d4d14ed6e4bd3534dc274d8cd
Dima Hlebnikov <lakone41@gmail.com>
2019-02-24 14:16:28 +0200

    [ci skip] add set and change delete instance

--- create_instance.sh
+++ create_instance.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 until gcloud container node-pools create ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${BUILD_TAG}:NoSchedule --node-labels=node=${BUILD_TAG} --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
--- delete_instance.sh
+++ delete_instance.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -x
 echo "------start script-----"
-until gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"
+EX=0
+until [ $EX -eq 1 ]
 do
- sleep 4
+    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER}${CHANGE_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
+        sleep 4
+        echo "Instance not found"
+        EX=1
+    else
+        sleep 4
+        EX=0
+    fi
 done
