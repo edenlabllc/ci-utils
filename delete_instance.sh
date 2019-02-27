4d900d087e2ef66f1b210d2bef49b9e3752e9cc9
liubenokvlad <47415779+liubenokvlad@users.noreply.github.com>
2019-02-27 23:12:08 +0200

    Update delete_instance.sh

--- delete_instance.sh
+++ delete_instance.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
 echo "------start script-----"
-echo "random value ${RANDOM_VAR}"
+echo "value ${GIT_COMMIT:0:7}"
 GIT_COMMIT=$(git rev-parse HEAD)
 EX=0
 until [ $EX -eq 1 ]
 do
-    if gcloud container node-pools delete ${PROJECT_NAME}-${RANDOM_VAR}-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
+    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
         sleep 4
         echo "Instance not found"
         EX=1
