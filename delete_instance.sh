566835f81635a10e26ea98d77a048432f5d38dc7
liubenokvlad <47415779+liubenokvlad@users.noreply.github.com>
2019-02-28 00:19:10 +0200

    Update delete_instance.sh

--- delete_instance.sh
+++ delete_instance.sh
@@ -6,7 +6,7 @@ GIT_COMMIT=$(git rev-parse HEAD)
 EX=0
 until [ $EX -eq 1 ]
 do
-    if gcloud container node-pools delete ${PROJECT_NAME}-${JENKINS_NODE_COOKIE:0:7}-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
+    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_BRANCH}-${BUILD_ID} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
         sleep 4
         echo "Instance not found"
         EX=1
