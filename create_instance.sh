2b935145f752b17448b976529c4f93a33afc249d
liubenokvlad <47415779+liubenokvlad@users.noreply.github.com>
2019-02-27 23:30:36 +0200

    Update create_instance.sh

--- create_instance.sh
+++ create_instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-until gcloud container node-pools create ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${GIT_COMMIT:0:7}:NoSchedule --node-labels=node=${GIT_COMMIT:0:7} --num-nodes=1 --zone=europe-west1-d --preemptible;
+until gcloud container node-pools create ${PROJECT_NAME}-${JENKINS_NODE_COOKIE}-${BUILD_NUMBER} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${GIT_COMMIT:0:7}:NoSchedule --node-labels=node=${GIT_COMMIT:0:7} --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
 done
