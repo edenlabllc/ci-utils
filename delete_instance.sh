b17fd2e2e0435bedaa69f01f52064c51c00b8df4
liubenokvlad <47415779+liubenokvlad@users.noreply.github.com>
2019-02-27 23:31:16 +0200

    Update delete_instance.sh

--- delete_instance.sh
+++ delete_instance.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
 echo "------start script-----"
-echo "value ${GIT_COMMIT:0:7}"
+echo "value ${JENKINS_NODE_COOKIE}"
 GIT_COMMIT=$(git rev-parse HEAD)
 EX=0
 until [ $EX -eq 1 ]
 do
-    if gcloud container node-pools delete ${PROJECT_NAME}-${GIT_COMMIT:0:7}-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
+    if gcloud container node-pools delete ${PROJECT_NAME}-$}JENKINS_NODE_COOKIE}-${BUILD_NUMBER} --zone=europe-west1-d --cluster=dev --quiet 2>&1 | grep -q "Not found"; then
         sleep 4
         echo "Instance not found"
         EX=1
