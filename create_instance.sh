e3930700d800a12ba7b3ef931706b2e647209fe3
Dima Hlebnikov <lakone41@gmail.com>
2019-02-28 15:00:26 +0200

    [ci skip] change create and delete

--- create_instance.sh
+++ create_instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-until gcloud container node-pools create ${PROJECT_NAME}-${RD} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${RD}:NoSchedule --node-labels=node=${RD} --num-nodes=1 --zone=europe-west1-d --preemptible;
+until gcloud container node-pools create ${RD} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${RD}:NoSchedule --node-labels=node=${RD} --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
 done
