4436e99ad221e8742a7ff67742ac7c538d7f574c
Dima Hlebnikov <lakone41@gmail.com>
2019-02-28 14:18:50 +0200

    fix create and delete instance

--- create_instance.sh
+++ create_instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-until gcloud container node-pools create ${PROJECT_NAME}-${BRANCH_NAME}-${BUILD_ID} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${PROJECT_NAME}-${BRANCH_NAME}-${BUILD_ID}:NoSchedule --node-labels=node=${PROJECT_NAME}-${BRANCH_NAME}-${BUILD_ID} --num-nodes=1 --zone=europe-west1-d --preemptible;
+until gcloud container node-pools create ${PROJECT_NAME}-${BUILD_ID} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${PROJECT_NAME}-${BUILD_ID}:NoSchedule --node-labels=node=${PROJECT_NAME}-${BUILD_ID} --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
 done
