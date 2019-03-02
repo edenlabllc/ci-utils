c3afc4b84c9dd4cd3cba5b83a424f407bfd7e750
liubenokvlad <liubenok@edenlab.com.ua>
2019-03-03 00:58:20 +0200

    [ci skip] update files

--- create_instance.sh
+++ create_instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-until gcloud container node-pools create ${RD} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=${RD}:NoSchedule --node-labels=node=${RD} --num-nodes=1 --zone=europe-west1-d --preemptible;
+until gcloud container node-pools create $PROJECT_NAME-$JOB_BASE_NAME-${RD:0:10} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME-$JOB_BASE_NAME-${RD:0:10}:NoSchedule --node-labels=node=$PROJECT_NAME-$JOB_BASE_NAME-${RD:0:10} --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
 done
