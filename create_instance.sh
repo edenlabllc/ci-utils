15bfbd2292268e66e986d63d2499a1bd2ed08be2
liubenokvlad <47415779+liubenokvlad@users.noreply.github.com>
2019-03-03 02:05:41 +0200

    Update create_instance.sh

--- create_instance.sh
+++ create_instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-until gcloud container node-pools create $PROJECT_NAME-$BUILD_ID-${RD:0:14} --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME-$BUILD_ID:NoSchedule --node-labels=node=$PROJECT_NAME-$BUILD_ID-${RD:0:14} --num-nodes=1 --zone=europe-west1-d --preemptible;
+until gcloud container node-pools create $PROJECT_NAME-$BUILD_ID-$RD_CROP --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME-$BUILD_ID-$RD_CROP:NoSchedule --node-labels=node=$PROJECT_NAME-$BUILD_ID-$RD_CROP --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
 done
