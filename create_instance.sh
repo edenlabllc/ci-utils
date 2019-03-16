64ba305aa832ad97903c4885037ef57675c5719c
liubenokvlad <47415779+liubenokvlad@users.noreply.github.com>
2019-03-16 02:46:44 +0200

    Update create_instance.sh

--- create_instance.sh
+++ create_instance.sh
@@ -3,7 +3,7 @@ set -x
 EX=0
 until [ $EX -eq 1 ]
 do
-    if gcloud container node-pools create $PROJECT_NAME --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME:PreferNoSchedule --node-labels=node=$PROJECT_NAME --num-nodes=1 --zone=europe-west1-d --preemptible 2>&1 | grep gke-dev-uaddresses |  grep -q "message=Already exists"; then
+    if gcloud container node-pools create $PROJECT_NAME --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME:PreferNoSchedule --node-labels=node=$PROJECT_NAME --num-nodes=1 --zone=europe-west1-d --preemptible 2>&1 | grep gke-dev-uaddresses |  grep "message=Already exists"; then
         sleep 4
         echo "Node already exist"
         EX=1
