84d8337607f3c31375a02668933f3ce030cdb153
liubenokvlad <liubenok@edenlab.com.ua>
2019-03-16 08:54:05 +0200

    [ci skip] update script create instance

--- create_instance.sh
+++ create_instance.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -x
-EX=0
-until [ $EX -eq 1 ]
+EX=1
+until [ $EX -eq 0 ]
 do
     if gcloud container node-pools create $PROJECT_NAME --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME:PreferNoSchedule --node-labels=node=$PROJECT_NAME --num-nodes=1 --zone=europe-west1-d --preemptible 2>&1 | grep gke-dev-uaddresses |  grep "message=Already"; then
         sleep 4
         echo "Node already exist"
-        EX=1
-    else
-        sleep 22
         EX=0
+    else
+        EX=1
+        until gcloud container node-pools create $PROJECT_NAME-$BUILD_ID-$RD_CROP --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME-$BUILD_ID-$RD_CROP:PreferNoSchedule --node-labels=node=$PROJECT_NAME-$BUILD_ID-$RD_CROP --num-nodes=1 --zone=europe-west1-d --preemptible;
+        do
+            sleep 22
+        done
     fi
 done
