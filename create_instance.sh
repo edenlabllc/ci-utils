a5394c95f724791f99972b331eefbdb210671448
liubenokvlad <liubenok@edenlab.com.ua>
2019-03-16 09:21:02 +0200

    [ci skip] update script create instance

--- create_instance.sh
+++ create_instance.sh
@@ -9,9 +9,13 @@ do
         EX=0
     else
         EX=1
-        until gcloud container node-pools create $PROJECT_NAME --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME:PreferNoSchedule --node-labels=node=$PROJECT_NAME --num-nodes=1 --zone=europe-west1-d --preemptible;
-        do
-            sleep 22
-        done
+        echo "second loop"
+        if gcloud container node-pools create $PROJECT_NAME --cluster=dev --machine-type=${INSTANCE_TYPE} --node-taints=ci=$PROJECT_NAME:PreferNoSchedule --node-labels=node=$PROJECT_NAME --num-nodes=1 --zone=europe-west1-d --preemptible 2>&1 | grep "done"; then
+            sleep 4
+            echo "Node  exist"
+            EX=0
+        else
+            EX=1
+        fi
     fi
 done
