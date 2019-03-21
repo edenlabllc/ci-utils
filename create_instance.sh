e7fc512fd28a1ce55bcc1de5726320784568600d
Dima Hlebnikov <41900337+dhlebnikov1@users.noreply.github.com>
2019-03-21 13:59:34 +0200

    add name var

--- create_instance.sh
+++ create_instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-until gcloud container node-pools create $PROJECT_NAME-$BUILD_ID --cluster=dev --machine-type=${INSTANCE_TYPE} --image-type=ubuntu --node-taints=ci=$RD_CROP:NoSchedule --node-labels=node=$RD_CROP --num-nodes=1 --zone=europe-west1-d --preemptible;
+until gcloud container node-pools create $PROJECT_NAME-$BUILD_ID-$NAME --cluster=dev --machine-type=${INSTANCE_TYPE} --image-type=ubuntu --node-taints=ci=$RD_CROP:NoSchedule --node-labels=node=$RD_CROP --num-nodes=1 --zone=europe-west1-d --preemptible;
 do
     sleep 22
 done
