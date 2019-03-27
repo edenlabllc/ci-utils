433d97007157e4ed322a9ef8370dc3f7fb12509d
Dima Hlebnikov <lakone41@gmail.com>
2019-03-27 17:47:29 +0200

    change wait for deployment

--- autodeploy.sh
+++ autodeploy.sh
@@ -33,7 +33,7 @@ if [ -z "$CHANGE_ID" ]; then
             if [ "$label" != "null" ]; then 
                 echo "kubectl delete pod -l app=$label -n $namespace"
                 kubectl delete pod -l app=$label -n $namespace
-                $TRAVIS_BUILD_DIR/wait-for-deployment.sh $deployment $namespace 180
+                ./wait-for-deployment.sh $deployment $namespace 180
                     if [ "$?" -eq 0 ]; then
                         kubectl get pod -l app=$label -n $namespace
                     else
