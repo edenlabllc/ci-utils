6809fae26f6d4f40e7eab00d8f7658f8e4364d69
liubenokvlad <liubenok@edenlab.com.ua>
2019-04-19 08:50:36 +0300

    [ci skip] update script

--- check-PR.sh
+++ check-PR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
           env;
               if [[ $BRANCH_NAME = te* ]] ; then
-                    if curl https://api.github.com/repos/edenlabllc/$REPOSITORY_NAME/pulls/$CHANGE_ID 2>/dev/null | json -a body | grep -Eq '#[0-9]{1,}' ; then
+                    if curl https://api.github.com/repos/edenlabllc/$REPOSITORY_NAME/pulls/$CHANGE_ID 2>/dev/null |  jq '.body' | grep -Eq '#[0-9]{1,}' ; then
                         echo "---------Correct PR and meet the requirements-------------"
                         exit 0
                     else
