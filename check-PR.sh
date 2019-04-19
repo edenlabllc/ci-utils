7170481a2d9237ec4e888700a908bece469acde6
liubenokvlad <liubenok@edenlab.com.ua>
2019-04-19 08:14:18 +0300

    [ci skip] create check script

---
+++ check-PR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+          env;
+              if [[ $BRANCH_NAME = te* ]] ; then
+                    if curl https://api.github.com/repos/edenlabllc/$REPOSITORY_NAME/pulls/$CHANGE_ID 2>/dev/null | json -a body | grep -Eq '#[0-9]{1,}' ; then
+                        echo "---------Correct PR and meet the requirements-------------"
+                        exit 0
+                    else
+                      echo "---------PR does not meet the requirements-----------"
+                            exit 1
+                    fi
+              fi
+              echo "------This is ordinary commit in branch, continue CI-------"
+              exit 0
