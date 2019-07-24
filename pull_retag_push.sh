de3a0102e6d079ac3833a53332478d762a928233
Volodymyr Shvaiko <cub4lt@Mgmail.com>
2019-07-24 15:48:12 +0300

    fix

--- pull_retag_push.sh
+++ pull_retag_push.sh
@@ -3,5 +3,5 @@
 set -x
 
 docker pull "edenlabllc/$1:develop"
-docker tag "edenlabllc/$1:develop" "edenlabllc/$1:$2"
-docker push "edenlabllc/$1:$2"
+docker tag "edenlabllc/$1:develop" "edenlabllc/$1:$2-rc2"
+docker push "edenlabllc/$1:$2-rc2"
