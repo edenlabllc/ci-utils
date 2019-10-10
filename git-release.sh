d069e1299c9aca25dfdd0b98331c7469a26b8e74
Dima Hlebnikov <lakone41@gmail.com>
2019-10-10 16:18:29 +0300

    [ci skip] add git script

--- git-release.sh
+++ git-release.sh
@@ -3,7 +3,7 @@
 RELEASE_VERSION=$(cat mix.exs | grep "@version" -m 1 | sed 's/[[:alpha:]]//g' | sed 's/@//g' | sed 's/ //g' | sed 's/"//g');
 until git checkout -b release_$RELEASE_VERSION;
 do
-  if [[ $RELEASE_VERSION != *"-rc" ]]; then
+  if [[ $RELEASE_VERSION != *"-rc"* ]]; then
     RELEASE_VERSION=$RELEASE_VERSION'-rc'
   elif [[ ${RELEASE_VERSION: -1} =~ ^-?[0-9]+$ ]]; then
     RC_VER=${RELEASE_VERSION: -1}
