665bd0719c48570083cc457baf19f78123971bf4
Dima Hlebnikov <lakone41@gmail.com>
2019-11-19 16:16:54 +0200

    remove -rc from tags for docker images

--- push-changes.sh
+++ push-changes.sh
@@ -10,7 +10,7 @@ if [[  -z "${CHANGE_ID}" && "${GIT_BRANCH}" == "develop" ]]; then
         echo "[E] You need to specify Docker Hub account"
         exit 1
     fi
-    
+
     APPS_LIST=$(echo ${APPS} | jq -r '.[].app');
     for app in ${APPS_LIST}
     do
@@ -51,44 +51,44 @@ elif [[  -z "${CHANGE_ID}" && "${GIT_BRANCH}" == "master" ]]; then
         PATCH=$(git log --oneline --since=${LAST_IMAGE_UPDATE} | sed 's/^ \+/&HEAD~/' | grep fix -c)
         CHORE=$(git log --oneline --since=${LAST_IMAGE_UPDATE} | sed 's/^ \+/&HEAD~/' | grep chore -c)
         MAJOR=$(git log --oneline --since=${LAST_IMAGE_UPDATE} | sed 's/^ \+/&HEAD~/' | grep ! -c)
-    
-        if [ "${MAJOR}" != "0" ]; then 
+
+        if [ "${MAJOR}" != "0" ]; then
             echo "MAJOR"; version="major"
             NEW_MAJOR=$((${LAST_MAJOR}+1))
-            NEW_MINOR=0   
+            NEW_MINOR=0
             NEW_PATCH=0
-        elif [ "${MINOR}" != "0" ]; then 
+        elif [ "${MINOR}" != "0" ]; then
             echo "MINOR"; version="minor"
             NEW_MAJOR=${LAST_MAJOR}
-            NEW_MINOR=$((${LAST_MINOR}+1))   
+            NEW_MINOR=$((${LAST_MINOR}+1))
             NEW_PATCH=0
-        elif [ "${PATCH}" != "0" ]; then 
+        elif [ "${PATCH}" != "0" ]; then
             echo "PATCH"; version="patch"
             NEW_MAJOR=${LAST_MAJOR}
-            NEW_MINOR=${LAST_MINOR}   
+            NEW_MINOR=${LAST_MINOR}
             NEW_PATCH=$((${LAST_PATCH}+1))
-        elif [ "${CHORE}" != "0" ]; then 
+        elif [ "${CHORE}" != "0" ]; then
             echo "PATCH"; version="patch"
             NEW_MAJOR=${LAST_MAJOR}
-            NEW_MINOR=${LAST_MINOR}   
+            NEW_MINOR=${LAST_MINOR}
             NEW_PATCH=$((${LAST_PATCH}+1))
         else echo "No change from last build in branch $GIT_BRANCH"; version="0"
         fi;
-        
+
         if [ "$version" != "0" ]; then
             NEW_VERSION="$NEW_MAJOR.$NEW_MINOR.$NEW_PATCH"
-            echo "[I] New version $DOCKER_NAMESPACE/$app:$NEW_VERSION" 
-            echo "[I] Now in test stage image will be taget with '-rc' $DOCKER_NAMESPACE/$app:$NEW_VERSION-rc" 
+            echo "[I] New version $DOCKER_NAMESPACE/$app:$NEW_VERSION"
+            echo "[I] Now in test stage image will be taget $DOCKER_NAMESPACE/$app:$NEW_VERSION"
             echo "[I] Pushing changes to Docker Hub.."
-            echo "docker tag \"${DOCKER_NAMESPACE}/${app}:$GIT_COMMIT\" \"${DOCKER_NAMESPACE}/${app}:$NEW_VERSION-rc\""
-            echo "docker push \"${DOCKER_NAMESPACE}/${app}:$NEW_VERSION-rc\""
-            sudo docker tag "${DOCKER_NAMESPACE}/${app}:$GIT_COMMIT" "${DOCKER_NAMESPACE}/${app}:$NEW_VERSION-rc"
-            echo "docker push \"${DOCKER_NAMESPACE}/${app}:$NEW_VERSION-rc\""
-            sudo docker push "${DOCKER_NAMESPACE}/${app}:$NEW_VERSION-rc"
+            echo "docker tag \"${DOCKER_NAMESPACE}/${app}:$GIT_COMMIT\" \"${DOCKER_NAMESPACE}/${app}:$NEW_VERSION\""
+            echo "docker push \"${DOCKER_NAMESPACE}/${app}:$NEW_VERSION\""
+            sudo docker tag "${DOCKER_NAMESPACE}/${app}:$GIT_COMMIT" "${DOCKER_NAMESPACE}/${app}:$NEW_VERSION"
+            echo "docker push \"${DOCKER_NAMESPACE}/${app}:$NEW_VERSION\""
+            sudo docker push "${DOCKER_NAMESPACE}/${app}:$NEW_VERSION"
         else echo "Nothing todo."
         fi;
     done
-   
+
 
 elif [[  -z "${CHANGE_ID}" && "${GIT_BRANCH}" =~ ^(test.*)$ ]]; then
     echo "This is ${GIT_BRANCH} for testing new feater. This branch building to image and will be push with tag ${GIT_BRANCH} and deploy to dev"
