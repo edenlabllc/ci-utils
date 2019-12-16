aa96bd805a21fbdfc2b6676c3c5323accb776caf
Dima Hlebnikov <lakone41@gmail.com>
2019-12-16 16:38:07 +0200

    changes for build

--- build-container.sh
+++ build-container.sh
@@ -13,12 +13,12 @@ for row in $(echo "${APPS}" | jq -c '.[]'); do
     echo "[I] Building a Docker container for '$APP_NAME' application";
     echo "docker build --tag \"${DOCKER_NAMESPACE}/$APP_NAME:$GITHUB_SHA\""
     echo "    --file \"${GITHUB_WORKSPACE}/${DOCKERFILE}\""
-    echo "    --build-arg APP_NAME=$APP_NAME"
+    echo "    --build-arg APP_NAME=$APP_NAME --build-arg SSH_PRI="\$SSH_PRIVATE_KEY" --build-arg SSH_PUB="\$SSH_PUB_KEY""
     echo "    \"$GITHUB_WORKSPACE\""
 
     sudo docker build --tag "${DOCKER_NAMESPACE}/$APP_NAME:$GITHUB_SHA" \
             --file "${GITHUB_WORKSPACE}/${DOCKERFILE}" \
-            --build-arg APP_NAME=$APP_NAME \
+            --build-arg APP_NAME=$APP_NAME --build-arg SSH_PRI="$SSH_PRIVATE_KEY" --build-arg SSH_PUB="$SSH_PUB_KEY" \
             "$GITHUB_WORKSPACE";
     echo
 done
