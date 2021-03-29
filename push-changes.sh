4f786b3dafc583fcfbbb8771665dbd68b4b58df1
dhlebnikov1 <lakone41@gmail.com>
2021-03-29 13:29:22 +0300

    fix: add new app me for demo autodeploy

--- push-changes.sh
+++ push-changes.sh
@@ -201,6 +201,9 @@ elif [[  -z "${CHANGE_ID}" && "${GIT_BRANCH}" == "master" ]]; then
                     elif [ "$APPS_LIST" == "replication_consumer" ]; then
                         sed -i'' -e "25,29s/tag:.*/tag: \"$NEW_VERSION\"/" "$chart/values-demo.yaml"
                         git add $chart/values-demo.yaml && git commit -m "bump $chart/$APPS_LIST to $NEW_VERSION" && git push origin master && cd .. && rm -rf ehealth.charts || true
+                    elif [ "$APPS_LIST" == "ops_data_consumer" ]; then
+                        sed -i'' -e "37,41s/tag:.*/tag: \"$NEW_VERSION\"/" "$chart/values-demo.yaml"
+                        git add $chart/values-demo.yaml && git commit -m "bump $chart/$APPS_LIST to $NEW_VERSION" && git push origin master && cd .. && rm -rf ehealth.charts || true
                     fi
                 elif [ "$chart" == "mithril" ]; then
                     if [ "$APPS_LIST" == "mithril_api" ]; then
